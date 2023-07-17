1bc6d0d117a361cd215aabe1369cf3cc7948d9b8
sydney-rd <sydneydavid56@gmail.com>
2023-07-16 22:02:53 -0400

    hamburger menu for homepage

--- components/projectCategories.js
+++ components/projectCategories.js
@@ -1,52 +1,43 @@
-import React, { useState } from 'react'
-import { VStack, useBreakpointValue, IconButton, Collapse } from '@chakra-ui/react'
-import { motion } from 'framer-motion'
-import { projects } from '../utilities/projects'
-import { HamburgerIcon } from '@chakra-ui/icons'
+import React, { useState } from 'react';
+import { VStack, useBreakpointValue, IconButton, Collapse } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
+import { projects } from '../utilities/projects';
+import { HamburgerIcon } from '@chakra-ui/icons';
 
-const MotionIconButton = motion(IconButton)
+const MotionIconButton = motion(IconButton);
 
-export default function ProjectCategories({
-  selectedCategory,
-  onCategoryClick
-}) {
-  const categories = [...new Set(projects.map(project => project.category))]
-  const isMobile = useBreakpointValue({ base: true, md: false })
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
+export default function ProjectCategories({ selectedCategory, onCategoryClick }) {
+  const categories = [...new Set(projects.map(project => project.category))];
+  const isMobile = useBreakpointValue({ base: true, md: false });
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const topPosition = useBreakpointValue({
     base: '2%',
-    md: '45%'
-  })
+    md: '45%',
+  });
 
   return (
     <VStack
-      display="flex"
-      flexDirection="column"
-      alignItems="flex-start"
-      justifyContent="center"
       position="fixed"
       top={topPosition}
       left="4"
-      backgroundColor="transparent"
       zIndex="10"
-      cursor="pointer"
       fontFamily="Verdana, Geneva, Tahoma, sans-serif"
       fontSize=".7rem"
       fontWeight="bold"
-      filter='brightness(150%)'
+      filter="brightness(150%)"
       spacing={6}
     >
-      {isMobile ? (
+      {isMobile && (
         <>
           <MotionIconButton
             icon={<HamburgerIcon />}
+            fontSize="1.2rem"
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }}
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 0.2 }}
-            userSelect='none'
             onClick={() => setIsMenuOpen(!isMenuOpen)}
             sx={{
               bg: 'transparent',
@@ -55,29 +46,28 @@ export default function ProjectCategories({
               _focus: { boxShadow: 'none' },
               _visited: { boxShadow: 'none' },
               _focusVisible: { boxShadow: 'none' },
-              _disabled: { cursor: 'not-allowed', opacity: 0.5 },
-              color: 'white'            }}
+              color: 'white',
+            }}
           />
 
           <Collapse in={isMenuOpen}>
             <VStack
               position="absolute"
-              userSelect='none'
-              top={isMobile ? '100%' : 'auto'}
+              userSelect="none"
+              top={isMobile ? '80%' : 'auto'}
               left={isMobile ? '0' : 'auto'}
               right={isMobile ? '0' : 'auto'}
-              backgroundColor="transparent"
-              boxShadow="md"
+              align="left"
+              whiteSpace="nowrap"
               zIndex="100"
-              mt="1"
+              spacing={4}
               p="2"
-              opacity={selectedCategory ? 1 : 0.7}
             >
               {categories.map((category, index) => {
                 const categoryColor =
                   selectedCategory === category
                     ? projects.find(project => project.category === category).color
-                    : 'white'
+                    : 'white';
 
                 return (
                   <motion.p
@@ -86,24 +76,28 @@ export default function ProjectCategories({
                     whileHover={{ scale: 1.1 }}
                     whileTap={{ scale: 0.9 }}
                     initial={{ opacity: 0 }}
-                    animate={{ opacity: selectedCategory === category ? 1 : 0.7, color: categoryColor }}
+                    animate={{
+                      opacity: selectedCategory === category ? 1 : 0.7,
+                      color: categoryColor,
+                    }}
                     transition={{ duration: 0.2 }}
                     className="projectCategories"
                   >
                     {category}
                   </motion.p>
-                )
+                );
               })}
             </VStack>
           </Collapse>
         </>
-      ) : (
-        // Render desktop version without hamburger menu
+      )}
+
+      {!isMobile &&
         categories.map((category, index) => {
           const categoryColor =
             selectedCategory === category
               ? projects.find(project => project.category === category).color
-              : 'white'
+              : 'white';
 
           return (
             <motion.p
@@ -112,15 +106,17 @@ export default function ProjectCategories({
               whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 0.9 }}
               initial={{ opacity: 0 }}
-              animate={{ opacity: selectedCategory === category ? 1 : 0.7, color: categoryColor }}
+              animate={{
+                opacity: selectedCategory === category ? 1 : 0.7,
+                color: categoryColor,
+              }}
               transition={{ duration: 0.2 }}
               className="projectCategories"
             >
               {category}
             </motion.p>
-          )
-        })
-      )}
+          );
+        })}
     </VStack>
-  )
+  );
 }
