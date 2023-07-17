a3d51c3b9f908722a6fdd54486b9d2982cea53ea
sydney-rd <sydneydavid56@gmail.com>
2023-07-17 15:38:58 -0400

    logo

--- components/spinningLogo.js
+++ components/spinningLogo.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Html } from '@react-three/drei';
+import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Html } from '@react-three/drei'
 import Logo from '../public/assets/logo/logo.png'
 
 const SpinningLogo = () => {
@@ -10,16 +10,28 @@ const SpinningLogo = () => {
       <pointLight position={[10, 10, 10]} />
       <OrbitControls autoRotate />
       <Html position={[0, 0, 0]} transform>
-        <div style={{ width: '100%', height: '50vh', display: 'flex', userSelect: 'none', paddingBottom: '8rem', paddingRight: '3.5rem', justifyContent: 'center', alignItems: 'center', userSelect: 'none' }}>
+        <div
+          style={{
+            width: '100%',
+            height: '50vh',
+            display: 'flex',
+            userSelect: 'none',
+            paddingBottom: '8rem',
+            paddingRight: '3.5rem',
+            justifyContent: 'center',
+            alignItems: 'center',
+            userSelect: 'none'
+          }}
+        >
           <img
-            src={Logo}
-            alt="Logo"
+            src={Logo.src}
+            alt='SDLogo'
             style={{ width: '80%', height: '70%' }}
           />
         </div>
       </Html>
     </Canvas>
-  );
-};
+  )
+}
 
-export default SpinningLogo;
+export default SpinningLogo
